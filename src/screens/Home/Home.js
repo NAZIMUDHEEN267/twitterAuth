73980818651e13b2df4165b5e745f481edcca91e
nazimudheen267 <nazimudheen267@gmail.com>
2022-11-25 18:19:51 +0530

    footer added to home component

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,13 +1,21 @@
-import { Text, View, Image, TouchableOpacity } from 'react-native';
+import {
+    Text,
+    View,
+    Image,
+    TouchableOpacity,
+    ScrollView
+} from 'react-native';
 import React, { Component } from 'react';
 import styles from "./Home.styles";
 import Icon from "react-native-vector-icons/Feather";
 import twitter from "Images/twitter.png";
+import Unsplash from 'unsplash-js';
 
 class Home extends Component {
+
     render() {
         return (
-            <View>
+            <View style={styles.parent}>
                 {/* nav */}
                 <View style={styles.nav}>
                     <TouchableOpacity>
@@ -19,8 +27,33 @@ class Home extends Component {
                     </TouchableOpacity>
                 </View>
                 {/* scrollView */}
+                <ScrollView style={styles.scrollContainer}>
+                    {
+                        Array.from("askdfjlasdkfsdfghsd").map((_, i) => {
+                            return (
+                                <TouchableOpacity style={styles.scrollItem} key={i}>
+                                    <Image source={twitter} style={styles.scrollItem_img} />
+                                </TouchableOpacity>
+                            )
+                        })
+                    }
+                </ScrollView>
                 {/* news */}
                 {/* footernav */}
+                <View style={[styles.nav, styles.footer]}>
+                    <TouchableOpacity>
+                        <Icon name="home" size={25} />
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Icon name="hash" size={25} />
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Icon name="search" size={25} />
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Icon name="mail" size={25} />
+                    </TouchableOpacity>
+                </View>
             </View>
         )
     }
--- src/screens/Home/Home.styles.js
+++ src/screens/Home/Home.styles.js
@@ -1,18 +1,42 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
+    parent: {
+        height: "100%",
+        width: "100%"
+    },
     nav: {
         width: "100%",
         height: 60,
         flexDirection: "row",
-        backgroundColor: "yellow",
+        backgroundColor: "#fff",
         alignItems: "center",
         justifyContent: "space-between",
-        paddingHorizontal: 10
+        paddingHorizontal: 10,
+        elevation: 10
     },
     navIconSize: {
         height: 25,
         width: 25
     },
-
+    scrollContainer: {
+        height: 60,
+        backgroundColor: "blue"
+    },
+    scrollItem: {
+        borderWidth: 2,
+        borderColor: "#0983ed",
+        borderRadius: 55,
+        padding: 5,
+        margin: 10
+    },
+    scrollItem_img: {
+        width: 40,
+        height: 40,
+        resizeMode: "contain"
+    },
+    footer: {
+        position: "absolute",
+        bottom: "0%"
+    }
 })
